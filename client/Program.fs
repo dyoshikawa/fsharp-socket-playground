ea106ebac0588ce3fd6413f7ceb39ac44cc93b6c
dyoshikawa <dyoshikawa1993@gmail.com>
2019-11-24 23:05:36 +0900

    fix

--- client/Program.fs
+++ client/Program.fs
@@ -5,34 +5,43 @@ open System.Text
 
 [<EntryPoint>]
 let main _argv =
+    printfn "送信する文字列を入力して下さい"
+    let input = stdin.ReadLine()
+    
     let socket = new Socket(AddressFamily.InterNetwork,
                             SocketType.Stream,
                             ProtocolType.Tcp)
     
     let endPoint = new IPEndPoint(IPAddress.Loopback, 8080)
+    
+    printfn "接続開始"
     socket.Connect(endPoint)
     
-    let buffer = Encoding.Default.GetBytes("test")
+    let sendBuf = Encoding.Default.GetBytes(input)
+    let sendBufLen = sendBuf.Length
     
-    let sendResult = socket.Send(buffer)
+    printfn "送信開始"
+    let sendResult = socket.Send(sendBuf)
     if sendResult = -1 then
         printfn "送信失敗"
         socket.Close()
         1
     else
+        printfn "送信完了"
+        printfn "受信開始"
         let memory = new MemoryStream()
-        let rec receive () =
-            let buffer = [| for _ in 1..10 -> byte(0) |]
-            let len = socket.Receive(buffer)
-            if len = 0 then
-                printfn "接続が切れました"
+        let rec receive (total : int) : unit =
+            if total >= sendBufLen then
+                printfn "受信完了"
+                printfn "接続終了"
                 socket.Close()
                 ()
             else
-//                printfn "%s" <| Encoding.Default.GetString(buffer, 0, len)
+                let buffer = [| for _ in 1..10 -> byte(0) |]
+                let len = socket.Receive(buffer)
                 memory.Write(buffer, 0, len)
-                receive ()
-        receive ()
-        printfn "%s" <| Encoding.Default.GetString(memory.GetBuffer())
+                receive (total + len)
+        receive 0
+        printfn "受信: %s" <| Encoding.Default.GetString(memory.GetBuffer())
         socket.Close()
         0
